36ea85ae93332f19d94d9853347db90a7df49658
DrewMas <andrewmasnyi@gmail.com>
2021-03-29 13:25:40 +0300

    CW till 81th row

--- Class work/classWork2.js
+++ Class work/classWork2.js
@@ -412,9 +412,12 @@
 // let arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // let arr5New = [];
 //
-// for (i = 0; i < arr5.length; i += 3) {
-//     if (i % 2 === 0) {
+// for (let  i = 0; i < arr5.length; i += 3) {
+//     if (arr5[i] % 2 === 0) {
 //         arr5New.push(arr5[i]);
 //     }
-//     console.log(arr5New);
 // }
+// console.log(arr5New);
+
+
+
