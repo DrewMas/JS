858a00a7b223e7eb555a6191d127e769f0b4f45c
DrewMas <andrewmasnyi@gmail.com>
2021-04-07 14:42:27 +0300

    CW 5 done

--- Class work/classWork5.js
+++ Class work/classWork5.js
@@ -95,4 +95,93 @@
 // newVolkswagen.changeYear(2015);
 // newVolkswagen.driver({Name: 'Misha', age: 24, car: false});
 
+// -створити класс попелюшка з полями ім'я, вік, розмір ноги
+// --Створити 10 попелюшок , покласти їх в масив
+// --Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+// -- за допоиоги циклу знайти яка попелюшка повинна бути з принцом
 
+// class Human {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+//
+// class Cinderella extends Human {
+//     constructor(name, age, footSize ) {
+//         super(name, age);
+//         this.footSize = footSize;
+//     }
+// }
+//
+// class Prince extends Human {
+//     constructor(name, age, shoeSize) {
+//         super(name, age,);
+//         this.shoeSize = shoeSize;
+//     }
+// }
+//
+// const cinderellas =  [
+//     new Cinderella('Anna', 20, 36),
+//     new Cinderella('Vika', 21, 37),
+//     new Cinderella('Dasha', 22, 38),
+//     new Cinderella('Ira', 23, 39),
+//     new Cinderella('Olya', 24, 38),
+//     new Cinderella('Yulia', 23, 35),
+//     new Cinderella('Nataliya', 22, 36),
+//     new Cinderella('Tanya', 21, 39),
+//     new Cinderella('Katya', 20, 38),
+//     new Cinderella('Vasylyna', 21, 40),
+//     ];
+//
+// const prince1 = new Prince('Petro', 25, 37);
+//
+// for (const cinderella of cinderellas) {
+//     if (cinderella.footSize === prince1.shoeSize) {
+//         console.log(cinderella);
+//     }
+// }
+
+
+
+// -створити функцію конструктор попелюшка з полями ім'я, вік, розмір ноги
+// --Створити 10 попелюшок , покласти їх в масив
+// --Сторити об'єкт типу "принц" за допомоги функції конструктора з полями ім'я, вік, туфелька яку він знайшов, та функцію "пошук попелюшки"
+// -- функція повинна приймати масив попелюшок, та шукає ту котра йому підходить
+
+function Cinderella(name, age, footSize) {
+    this.name = name;
+    this.age = age;
+    this.footSize = footSize;
+}
+
+let cinderellas =  [
+    new Cinderella('Anna', 20, 36),
+    new Cinderella('Vika', 21, 37),
+    new Cinderella('Dasha', 22, 38),
+    new Cinderella('Ira', 23, 39),
+    new Cinderella('Olya', 24, 38),
+    new Cinderella('Yulia', 23, 35),
+    new Cinderella('Nataliya', 22, 36),
+    new Cinderella('Tanya', 21, 39),
+    new Cinderella('Katya', 20, 38),
+    new Cinderella('Vasylyna', 21, 40),
+    ];
+
+function Prince(name, age, shoeSize) {
+    this.name = name;
+    this.age = age;
+    this.shoeSize = shoeSize;
+}
+let prince1 = new Prince('Petro', 25, 37);
+
+function findCinderella() {
+    for (let i = 0; i < cinderellas.length; i++) {
+        if (cinderellas[i].footSize === prince1.shoeSize) {
+            console.log(cinderellas[i].name);
+        }
+
+    }
+}
+
+findCinderella(cinderellas);
